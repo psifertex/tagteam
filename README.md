79ec3d4ecccd93948eb2086b2be5335b824a53f5
Jordan Wiens <jordan@psifertex.com>
2021-03-04 19:20:28 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# FUNctional Tags (v1.0)
+Author: **psifertex**
+
+_Tags functions using emoji for a number of interesting characteristics_
+
+## Description:
+
+LONG DESCRIPTION HERE ONCE I GET AN IMAGE
+
+
+## Installation Instructions
+
+### Darwin
+
+
+
+### Windows
+
+
+
+### Linux
+
+
+
+## Minimum Version
+
+This plugin requires the following minimum version of Binary Ninja:
+
+* 1555
+
+
+## License
+
+This plugin is released under a Apache-2.0 license.
+## Metadata Version
+
+2
---
+++ __init__.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+from binaryninja import PluginCommand, Settings, interaction, log_info, HighLevelILOperation
+
+Settings().register_group("tagteam", "Tag Team Plugin")
+Settings().register_setting("tagteam.largeFunc", """
+	{
+		"title" : "Number of basic blocks",
+		"type" : "number",
+		"default" : 40,
+		"description" : "Functions with more than this number of basic blocks will be considered large"
+	}
+	""")
+
+Settings().register_setting("tagteam.largeSwitch", """
+	{
+		"title" : "Large Switch Statement",
+		"type" : "number",
+		"default" : 10,
+		"description" : "Functions with switch statements with more than this many cases are considered switchy"
+	}
+	""")
+
+Settings().register_setting("tagteam.complexFunc", """
+	{
+		"title" : "Complex Function",
+		"type" : "number",
+		"default" : 40,
+		"description" : "Functions with cyclomatic complexity scores above this will be considered complex"
+	}
+	""")
+
+def init_tags(bv):
+	for tagType in tags:
+		if tagType['name'] not in bv.tag_types.keys():
+			bv.create_tag_type(tagType['name'], tagType['emoji'])
+
+def cc(fn):
+	nodes = len(fn.basic_blocks)
+	edges = sum(len(x.outgoing_edges) for x in fn.basic_blocks)
+	connected = 1 #always 1 for binary control flow graphs, kinda the whole point
+	return edges - nodes + 2 * connected
+
+def isswitchy(fn):
+	for h in fn.hlil.instructions:
+		if h.operation == HighLevelILOperation.HLIL_SWITCH:
+			return len(h.cases) > Settings().get_integer("tagteam.largeSwitch")
+	return False
+
+def iscomplex(fn):
+	return cc(fn) > Settings().get_integer("tagteam.complexFunc")
+
+def isleaf(fn):
+	return len(fn.callees) == 0
+
+def islarge(fn):
+	return len(fn.basic_blocks) >= Settings().get_integer("tagteam.largeFunc")
+
+def isstub(fn):
+	"""Returns true if a function is likely only a stub"""
+	if len(fn.basic_blocks) > 1 or len(fn.llil.basic_blocks) > 1:
+		return False
+	if fn.llil.basic_blocks[0].has_undetermined_outgoing_edges or len(fn.callees) == 1:
+		return True
+	return False
+
+def hasloop(fn):
+	"""Returns true if a function has a 'strange loop' (ignore this, inside joke)"""
+	for bb in fn.basic_blocks:
+		if bb in bb.dominance_frontier:
+			return True
+	return False
+
+tags = [ \
+{'emoji': '🍃', 'name': 'Leaf Function', 'description': 'Leaf function (does not call anything else)', 'fn': isleaf},
+{'emoji': '🔄', 'name': 'Loop Function', 'description': 'Function contains a loop', 'fn': hasloop},
+{'emoji': '🥾', 'name': 'Stub Function', 'description': 'Function is likely a stub (only contains one basic block and one call or indirect jump)', 'fn': isstub},
+{'emoji': '🐘', 'name': 'Large Function', 'description': 'Function is "large" (IE, it has more than the blocks defined above)', 'fn': islarge},
+{'emoji': '🤯', 'name': 'Complex Function', 'description': 'Function is "complex" (IE, it has a cyclomatic complexity greater than a defined constant)', 'fn': iscomplex},
+{'emoji': '🌴', 'name': 'Switchy Function', 'description': 'Function contains a large switch statemen', 'fn': isswitchy},
+]
+
+def start(bv):
+	init_tags(bv)
+	for fn in bv.functions:
+		for tagType in tags:
+			if tagType['fn'](fn):
+				fn.create_user_function_tag(bv.tag_types[tagType['name']], '', unique=True)
+				#fn.name = fn.name + tagType['emoji']
+
+PluginCommand.register("TagTeam", "Tag Functions with Emoji for Various Properties", start)
